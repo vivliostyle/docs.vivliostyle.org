222520b66a7a8c47f47ded1798c2084be1757e12
Yasuaki Uechi <y@uechi.io>
2019-12-26 01:44:43 +0900

    add docusaurus base

---
+++ docusaurus.config.js
@@ -0,0 +1,90 @@
+module.exports = {
+  title: "Vivliostyle",
+  tagline: "Vivliostyle-documentation",
+  url: "https://docs.vivliostyle.org",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "vivliostyle", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Vivliostyle",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg"
+      },
+      links: [
+        { to: "docs/user-guide", label: "User Guide", position: "left" },
+        { to: "docs/api", label: "API", position: "left" },
+        {
+          href: "https://github.com/vivliostyle/vivliostyle",
+          label: "GitHub",
+          position: "right"
+        }
+      ]
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "User Guide",
+              to: "docs/user-guide"
+            },
+            {
+              label: "API References",
+              to: "docs/api"
+            }
+          ]
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus"
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus"
+            }
+          ]
+        },
+        {
+          title: "Social",
+          items: [
+            {
+              label: "Blog",
+              to: "blog"
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus"
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus"
+            }
+          ]
+        }
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
+    }
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/facebook/docusaurus/edit/master/website/"
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      }
+    ]
+  ]
+};
---
+++ sidebars.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  someSidebar: {
+    "User Guide": ["user-guide"],
+    API: [
+      "api",
+      "supported-features",
+      "available-properties",
+      "typescript-migration"
+    ]
+  }
+};
